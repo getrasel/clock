27f08b98b3f3caa97efc2eab790551c6cbe5bea2
Rasel <mdraselahmed063@gmail.com>
2024-11-04 13:08:41 +0600

    clock

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let clockTimer;
+
+function startClock() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+
+    m = timeProper(m);
+    s = timeProper(s);
+
+    document.getElementById('hour').innerHTML = h;
+    document.getElementById('minute').innerHTML = m;
+    document.getElementById('sec').innerHTML = s;
+
+    // Store the timer ID so it can be cleared later
+    clockTimer = setTimeout(startClock, 1000);
+
+    function timeProper(i) {
+        if (i < 10) { i = "0" + i; }
+        return i;
+    }
+}
+
+
+
+function stop(){
+    clearTimeout(clockTimer);
+}
+function play(){
+    startClock();
+}
